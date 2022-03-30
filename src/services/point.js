a336a69321b858f859cd98550849ee6813ba1b8b
JacobK5 <jkoep@gratisites.com>
2022-03-30 16:45:49 -0600

    Made fixes to line up with client
    Woo hoo! It's pretty much all done now!

--- src/services/point.js
+++ src/services/point.js
@@ -8,31 +8,30 @@ class PointService {
   getMin = (earliest, latest) => {
     return Point.find({
       type: "min",
-      time: { $gte: earliest },
-      time: { $lte: latest },
-    }).exec();
+      time: { $gte: earliest, $lte: latest },
+    })
+      .sort({ time: 1 })
+      .exec();
   };
 
   getHour = (earliest, latest) => {
     return Point.find({
       type: "hour",
-      time: { $gte: earliest },
-      time: { $lte: latest },
+      time: { $gte: earliest, $lte: latest },
     }).exec();
   };
 
   getDay = (earliest, latest) => {
     return Point.find({
       type: "day",
-      time: { $gte: earliest },
-      time: { $lte: latest },
+      time: { $gte: earliest, $lte: latest },
     }).exec();
   };
 
   add = (gasPrice, time, type) => {
-    return new Point({ gasPrice, time, type }).save().then((pt) => {
-      console.log("saved", pt);
-    });
+    // return new Point({ gasPrice, time, type }).save().then((pt) => {
+    //   console.log("saved", pt);
+    // });
   };
 }
 
